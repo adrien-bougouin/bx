7632da0df684ba1759353b05986846793a15ab81
Adrien Bougouin <adrien.bougouin@gmail.com>
2026-04-15 23:54:36 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bake
+
+Make-ish bash-only build automation and task running tool.
---
+++ lib/engine.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+__BAKE_RECIPES__=()
+__BAKE_DEFAULT__=""
+
+bake:engine:load () {
+  while IFS='' read -r recipe_definition; do
+    local recipe="${recipe_definition#"declare -f "}"
+
+    [[ "${recipe}" =~ ^bake: ]] && continue
+
+    # Annotation functions #####################################################
+    @default () { export __BAKE_DEFAULT__="${recipe}"; }
+
+    @as () { true; }
+
+    @from () { true; }
+    ############################################################################
+
+    # FIXME: extract first line and $(eval ...)
+    [[ "$(declare -f "${recipe}")" =~ "@default" ]] && __BAKE_DEFAULT__="${recipe}"
+
+    __BAKE_RECIPES__+=("${recipe}")
+  done < <(declare -F)
+
+  # Annotation functions no longer needed ######################################
+  @default () { true; }
+  @as      () { true; }
+  @from    () { true; }
+  ##############################################################################
+}
+
+bake:engine:list () {
+  [[ ${#__BAKE_RECIPES__[@]} -eq 0 ]] && return
+
+  echo "Recipes:"
+  for r in "${__BAKE_RECIPES__[@]}"; do
+    echo "- ${r}"
+  done
+}
+
+bake:engine:exec () {
+  local recipe=$1
+  local args=("${@:2}")
+
+  printf "$(tput bold)%s%s$(tput sgr0)\n" "${recipe}" "${args+" ${args[*]}"}"
+
+  (eval "${recipe}" "${args+"${args[@]}"}")
+}
