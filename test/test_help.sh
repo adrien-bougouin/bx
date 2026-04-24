65bb25411cea3d30de2863261ccb8ba88d4208c1
Adrien Bougouin <adrien.bougouin@gmail.com>
2026-04-24 20:59:00 +0900

    test: help

---
+++ test/test_help.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+assert_output "bake -f ./bakefiles/help_no_recipe.bakefile -h" "$(
+  cat <<-EXPECTED
+		Usage: bake [options] [recipe] ...
+
+		Options:
+		    -f FILE, --file FILE, --bakefile FILE
+		       Read FILE as a bakefile.
+		    -h, --help
+		       Show this help.
+	EXPECTED
+)"
+
+assert_output "bake -f ./bakefiles/help.bakefile -h" "$(
+  cat <<-EXPECTED
+		Usage: bake [options] [recipe] ...
+
+		Options:
+		    -f FILE, --file FILE, --bakefile FILE
+		       Read FILE as a bakefile.
+		    -h, --help
+		       Show this help.
+
+		Available recipes:
+		    recipe_1
+		    recipe_2
+		    recipe_3
+	EXPECTED
+)"
+
+assert_output "bake -f ./bakefiles/help.bakefile --help" "$(
+  cat <<-EXPECTED
+		Usage: bake [options] [recipe] ...
+
+		Options:
+		    -f FILE, --file FILE, --bakefile FILE
+		       Read FILE as a bakefile.
+		    -h, --help
+		       Show this help.
+
+		Available recipes:
+		    recipe_1
+		    recipe_2
+		    recipe_3
+	EXPECTED
+)"
