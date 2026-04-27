d8f2eaa73a9ce430ad4fe9b6a8eed3c291b0757e
Adrien Bougouin <adrien.bougouin@gmail.com>
2026-04-27 22:07:01 +0900

    test: add tests fore 'require:' annotation

---
+++ test/test_require_recipe.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+skip assert_output "bake -q -f ./bakefiles/require.bakefile one-dependent" "$(
+	cat <<-EXPECTED
+		- required
+		- dependent
+	EXPECTED
+)"
+
+skip assert_output "bake -q -f ./bakefiles/require.bakefile multi-dependent" "$(
+	cat <<-EXPECTED
+		- required
+		- required
+		- required
+		- dependent
+	EXPECTED
+)"
+
+skip assert_output "bake -q -f ./bakefiles/require.bakefile multi-dependent--with-args" "$(
+	cat <<-EXPECTED
+		- required #1
+		- required #2
+		- required #3
+		- dependent
+	EXPECTED
+)"
