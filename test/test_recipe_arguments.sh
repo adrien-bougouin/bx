feab81f73983e4396a1747cdd411084c580f7aa2
Adrien Bougouin <adrien.bougouin@gmail.com>
2026-05-05 23:38:32 +0900

    test: add argument test

---
+++ test/test_recipe_arguments.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+assert_output "bake -q -f '${__BAKEFILES__}/recipe_arguments.bakefile' 'count-args'" "$(
+	cat <<-EXPECTED
+		0 argument(s)
+	EXPECTED
+)"
+
+assert_output "bake -q -f '${__BAKEFILES__}/recipe_arguments.bakefile' 'count-args a'" "$(
+	cat <<-EXPECTED
+		1 argument(s)
+	EXPECTED
+)"
+
+assert_output "bake -q -f '${__BAKEFILES__}/recipe_arguments.bakefile' 'count-args a b'" "$(
+	cat <<-EXPECTED
+		2 argument(s)
+	EXPECTED
+)"
+
+assert_output "bake -q -f '${__BAKEFILES__}/recipe_arguments.bakefile' 'count-args a b c'" "$(
+	cat <<-EXPECTED
+		3 argument(s)
+	EXPECTED
+)"
+
+assert_output "bake -q -f '${__BAKEFILES__}/recipe_arguments.bakefile' \"count-args 'a b c'\"" "$(
+	cat <<-EXPECTED
+		1 argument(s)
+	EXPECTED
+)"
