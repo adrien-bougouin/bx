7a567e4a425704da19a0c9952d7851530a824212
Adrien Bougouin <adrien.bougouin@gmail.com>
2026-06-29 23:55:29 +0900

    tests: fix unified test case structure

--- tests/cases/annotations/test_require_recipe.sh
+++ tests/cases/annotations/test_require_recipe.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
-assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require.bakefile' one-dependent" "$(
+assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' one-dependent" "$(
 	cat <<-EXPECTED
 		- required
 		- dependent
 	EXPECTED
 )"
 
-assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require.bakefile' one-dependent--subprocess" "$(
+assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' one-dependent--subprocess" "$(
 	cat <<-EXPECTED
 		- required
 		- dependent
 	EXPECTED
 )"
 
-assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require.bakefile' multi-dependent" "$(
-	cat <<-EXPECTED
-		- required
-		- required
-		- required
-		- dependent
-	EXPECTED
-)"
-
-assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require.bakefile' multi-dependent--subprocess" "$(
+assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' multi-dependent" "$(
 	cat <<-EXPECTED
 		- required
 		- required
@@ -32,7 +23,16 @@ assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require.bakefi
 	EXPECTED
 )"
 
-assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require.bakefile' multi-dependent--with-args" "$(
+assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' multi-dependent--subprocess" "$(
+	cat <<-EXPECTED
+		- required
+		- required
+		- required
+		- dependent
+	EXPECTED
+)"
+
+assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' multi-dependent--with-args" "$(
 	cat <<-EXPECTED
 		- required #1
 		- required #2
@@ -41,7 +41,7 @@ assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require.bakefi
 	EXPECTED
 )"
 
-assert_stderr "TERM= bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require__circular.bakefile' circular-dependent" "$(
+assert_stderr "TERM= bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe__circular.bakefile' circular-dependent" "$(
 	cat <<-EXPECTED
 		bake: Circular requirement for recipe 'circular-dependent'!
 	EXPECTED
