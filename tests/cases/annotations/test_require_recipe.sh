9637856025632b4863765c4033aa0bb4c9e3aa9f
Adrien Bougouin <adrien.bougouin@gmail.com>
2026-07-10 13:06:23 +0900

    test: update recipe requirement tests to catch recently fixed issue

--- tests/cases/annotations/test_require_recipe.sh
+++ tests/cases/annotations/test_require_recipe.sh
@@ -1,54 +1,70 @@
 #!/bin/bash
 
-assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' one-dependent" "$(
+assert_stdout "TERM= bake -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' one-dependent" "$(
 	cat <<-EXPECTED
+		require
 		- required
+		one-dependent
 		- dependent
 	EXPECTED
 )"
 
-assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' one-dependent--subprocess" "$(
+assert_stdout "TERM= bake -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' one-dependent--subprocess" "$(
 	cat <<-EXPECTED
+		require
 		- required
+		one-dependent--subprocess
 		- dependent
 	EXPECTED
 )"
 
-assert_stderr "TERM= bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' one-dependent--missing" "$(
+assert_stderr "TERM= bake -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' one-dependent--missing" "$(
 	cat <<-EXPECTED
 		bake: No recipe 'missing'!
 	EXPECTED
 )"
 
-assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' multi-dependent" "$(
+assert_stdout "TERM= bake -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' multi-dependent" "$(
 	cat <<-EXPECTED
+		require
 		- required
+		require
 		- required
+		require
 		- required
+		multi-dependent
 		- dependent
 	EXPECTED
 )"
 
-assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' multi-dependent--subprocess" "$(
+assert_stdout "TERM= bake -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' multi-dependent--subprocess" "$(
 	cat <<-EXPECTED
+		require
 		- required
+		require
 		- required
+		require
 		- required
+		multi-dependent--subprocess
 		- dependent
 	EXPECTED
 )"
 
-assert_stderr "TERM= bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' multi-dependent--missing" "$(
+assert_stderr "TERM= bake -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' multi-dependent--missing > /dev/null" "$(
 	cat <<-EXPECTED
 		bake: No recipe 'missing'!
 	EXPECTED
 )"
 
-assert_stdout "bake -q -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' multi-dependent--with-args" "$(
+assert_stdout "TERM= bake -f '${__TEST_BAKEFILES_PATH__}/annotations/require_recipe.bakefile' multi-dependent--with-args" "$(
 	cat <<-EXPECTED
+		require--with-args 1
 		- required #1
+		require--with-args 2
 		- required #2
+		require--with-args 3
 		- required #3
+		multi-dependent--with-args
 		- dependent
 	EXPECTED
 )"
