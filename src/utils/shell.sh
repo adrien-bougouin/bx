183b617598e293f412f6f2e826893677361e1f67
Adrien Bougouin <adrien.bougouin@gmail.com>
2026-09-09 09:32:10 +0900

    docs: add Google-style docs in bash utils

--- src/utils/shell.sh
+++ src/utils/shell.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
+#
+# Shell option management utilities for the bx tool.
 
 # FIXME: This only support single letter options. `pipefail` won't be captured.
 __BX_ORIGINAL_SHOPTS__="$-"
 
 readonly __BX_ORIGINAL_SHOPTS__
 
+################################################################################
+# Reset the current shell options to those captured when this file was sourced.
+#
+# Side effects:
+#   Re-enables the shell options captured at source time via `set`.
+################################################################################
 _bx::utils::shell::reset_options() {
   _bx::utils::shell::restore_options "${__BX_ORIGINAL_SHOPTS__}"
 }
 
+################################################################################
+# Restore a specific set of shell options.
+#
+# First disables every currently-set option, then enables the options present
+# in the given shopts string (in the same format as `${-}`).
+#
+# Arguments:
+#   shopts - The shell options to restore.
+#
+# Side effects:
+#   Modifies the current shell options via `set`.
+################################################################################
 _bx::utils::shell::restore_options() {
   local shopts="$1"
 
--- src/utils/string.sh
+++ src/utils/string.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+#
+# String manipulation utilities for the bx tool.
 
+################################################################################
+# Trim a pattern from both the start and end of a string.
+#
+# Repeatedly strips the given pattern from the beginning and end of the input
+# string until no further occurrences remain.
+#
+# Arguments:
+#   input   - The string to trim.
+#   pattern - The pattern to strip from both ends of the string.
+#
+# Outputs:
+#   Writes the trimmed string to stdout.
+################################################################################
 _bx::utils::string::trim() {
   local input="$1"
   local pattern="$2"
