c2ea2ca9dbe1a9a674d6a22f7a10ba287ef55aae
Adrien Bougouin <adrien.bougouin@gmail.com>
2026-09-10 13:27:38 +0900

    perf: early annotation parsing stop (taking a leap of face on bash presenting consistency)

--- src/annotations.sh
+++ src/annotations.sh
@@ -28,21 +28,31 @@ _bx::recipe::load_annotations() {
   _bx::annotation_parsing_stack::push "${recipe}"
 
   local line
+  local line_count=0
   while IFS='' read -r line; do
+    ((line_count++))
+
+    # Skip the first two lines, which are part of the recipe's function
+    # declaration.
+    [[ ${line_count} -gt 2 ]] || continue
+
     # Trim spaces.
     line="${line#"${line%%[![:space:]]*}"}"
-    # Strip subshell surroundings (e.g. '(  @default;').
+
+    # If the recipe is declared as a subshell, there are additional parentheses,
+    # on the first and last line, that need to be striped.
     line="${line#\(}"
     line="${line%\)}"
-    # Trim spaces again.
     line="${line#"${line%%[![:space:]]*}"}"
 
-    local line_head
+    local annotation_candidate_name
 
-    line_head="${line%% *}"
-    line_head="${line_head%;}"
+    annotation_candidate_name="${line%% *}"
+    annotation_candidate_name="${annotation_candidate_name%;}"
 
-    _bx::annotations::include "${line_head}" || continue
+    # Annotations should be first in the recipe; bx can stop parsing annotations
+    # as soon as it reaches a regular instruction.
+    _bx::annotations::include "${annotation_candidate_name}" || break
 
     eval "${line}"
   done < <(declare -f "${recipe}")
